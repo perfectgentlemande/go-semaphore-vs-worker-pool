d884e548fa2defa719b3653082bd4ef6e1f1dde6
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-07-11 14:13:49 +0400

    added semaphore with tests

--- semaphore/semaphore.go
+++ semaphore/semaphore.go
@@ -1,1 +1,65 @@
 package semaphore
+
+import (
+	"fmt"
+
+	"github.com/perfectgentlemande/go-semaphore-vs-worker-pool/users"
+)
+
+type Semaphore struct {
+	C chan struct{}
+}
+
+func (s *Semaphore) Acquire() {
+	s.C <- struct{}{}
+}
+
+func (s *Semaphore) Release() {
+	<-s.C
+}
+
+type resultWithError struct {
+	User users.User
+	Err  error
+}
+
+func DeactivateUsers(usrs []users.User, gCount int) ([]users.User, error) {
+	sem := Semaphore{
+		C: make(chan struct{}, gCount),
+	}
+
+	outputCh := make(chan resultWithError, len(usrs))
+	sgnlCh := make(chan struct{})
+
+	output := make([]users.User, 0, len(usrs))
+
+	for _, v := range usrs {
+		go func(usr users.User) {
+			sem.Acquire()
+			defer sem.Release()
+
+			err := usr.Deactivate()
+
+			select {
+			case outputCh <- resultWithError{
+				User: usr,
+				Err:  err,
+			}:
+			case <-sgnlCh:
+				return
+			}
+		}(v)
+	}
+
+	for i := len(usrs); i > 0; i-- {
+		resErr := <-outputCh
+		if resErr.Err != nil {
+			close(sgnlCh)
+			return nil, fmt.Errorf("an error occurred: %w", resErr.Err)
+		}
+
+		output = append(output, resErr.User)
+	}
+
+	return output, nil
+}
---
+++ semaphore/semaphore_test.go
@@ -0,0 +1,79 @@
+package semaphore
+
+import (
+	"reflect"
+	"sort"
+	"strconv"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/perfectgentlemande/go-semaphore-vs-worker-pool/users"
+)
+
+func generateUsers(count int) (activated, deactivated []users.User) {
+	activated = make([]users.User, count)
+	deactivated = make([]users.User, count)
+
+	for i := 0; i < count; i++ {
+		activated[i] = users.User{
+			ID:       uuid.New(),
+			Fullname: "Fullname " + strconv.Itoa(i),
+			Username: "Username " + strconv.Itoa(i),
+			Active:   true,
+		}
+		deactivated[i] = users.User{
+			ID:       activated[i].ID,
+			Fullname: activated[i].Fullname,
+			Username: activated[i].Username,
+			Active:   false,
+		}
+	}
+
+	return activated, deactivated
+}
+
+func TestDeactivateUsers(t *testing.T) {
+	activated, deactivated := generateUsers(10000)
+
+	type TestCase struct {
+		Activated   []users.User
+		Deactivated []users.User
+	}
+
+	cases := []TestCase{
+		{
+			Activated:   activated[:1],
+			Deactivated: deactivated[:1],
+		},
+		{
+			Activated:   activated[:10],
+			Deactivated: deactivated[:10],
+		},
+		{
+			Activated:   activated[:100],
+			Deactivated: deactivated[:100],
+		},
+		{
+			Activated:   deactivated[:1000],
+			Deactivated: deactivated[:1000],
+		},
+		{
+			Activated:   deactivated[:10000],
+			Deactivated: deactivated[:10000],
+		},
+	}
+
+	for i := range cases {
+		got, _ := DeactivateUsers(cases[i].Activated, 10)
+		sort.Slice(cases[i].Deactivated, func(j, k int) bool {
+			return cases[i].Deactivated[j].Fullname < cases[i].Deactivated[k].Fullname
+		})
+		sort.Slice(got, func(j, k int) bool {
+			return got[j].Fullname < got[k].Fullname
+		})
+
+		if !reflect.DeepEqual(got, cases[i].Deactivated) {
+			t.Errorf("expected: %v got: %v", cases[i].Deactivated, got)
+		}
+	}
+}
